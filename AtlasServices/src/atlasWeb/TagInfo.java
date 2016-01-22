299a60a45d368cafa2dbbc856f918467c7abe353
alon <shomratalon@gmail.com>
2016-01-22 13:12:29 +0200

    Init web app

---
+++ AtlasServices/src/atlasWeb/TagInfo.java
@@ -0,0 +1,30 @@
+package atlasWeb;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import atlasService.Location;
+import atlasService.Localization;
+
+public class TagInfo{
+	
+	private long id;
+
+	public TagInfo(long id){
+		this.id = id;
+	}
+	
+	public ArrayList<Long> get_x_coor(long tag){
+		ArrayList<Long> ret_list = new ArrayList<Long>();
+		//ArrayList<Location> tag_locations = 
+		return null;
+	}
+	
+	public void get_y_coor(long tag){
+		
+	}
+	
+	
+}
