99c3e1035dc6e4e8adffa4d6702c840cc30e6ca1
Alon Shomrat <shomratalon@gmail.com>
2016-02-16 20:58:45 +0200

    changed chart api

--- AtlasServices/WebContent/atlasMain.js
+++ AtlasServices/WebContent/atlasMain.js
@@ -10,20 +10,20 @@ $(document).ready(function() {
 		$('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
 			localStorage.setItem('activeTab', $(e.target).attr('href'));
 		});
+		document.getElementById('refreshButton').disabled=true;
 		var activeTab = localStorage.getItem('activeTab');
 		if(activeTab){
 			$('#MyTabs a[href="' + activeTab + '"]').tab('show');
 		}  
 	}
 	else {
-		window.location = "http://localhost:8080/AtlasServices/login.html"; 
+		window.location = "login.html"; 
 	}
 });
 
-
 function logout(){
 	loggedin=false;
-	window.location = "http://localhost:8080/AtlasServices/login.html"; 
+	window.location = "login.html"; 
 }
 function ajaxGetDetections(time) {
     var retData = null;
@@ -65,106 +65,39 @@ function refreshData(){
 }
 function showData(startTime, endTime){
 
-	var DELIMITER=1000;
-	var scaleXvalues=startTime+":"+endTime+":"+DELIMITER;
+	var DELIMITER=60000;
 	var time;
 	var res;
 	
 	time = {startTime: startTime, endTime: endTime};
 	res = window.ajaxGetDetections(time);    
-	var NumOfDetections = res["detectionsArr"];
+	var NumOfDetections = res["detectionsArr"];	
+      
+	var rows = [];
+	var j=0;
+	for (var i = startTime; i < endTime+1; i=i+DELIMITER) {
+		var date = (new Date(i)).toLocaleString();;
+		rows.push([date, NumOfDetections[j]]);
+		j++;
+	}
 	
-	//var NumOfDetections=[0, 0, 0, 0, 0, 0, 0, 0, 0, startTime%900, startTime%800, startTime%900, startTime%900, startTime%990, 0];
-	
-	zingchart.THEME = "classic";
-	var myConfig = {
-      "background-color": "white",
-      "type": "line",
-      "title": {
-        "text": "Atlas Monitor",
-        "color": "#333",
-        "background-color": "white",
-        "width": "60%",
-        "text-align": "left",
-      },
-      "subtitle": {
-        "text": "Toggle a data item to remove the series and adjust the scale.",
-        "text-align": "left",
-        "width": "60%"
-      },
-      "legend": {
-        "layout": "x1",
-        "margin-top": "5%",
-        "border-width": "0",
-        "shadow": false,
-        "marker": {
-          "cursor": "hand",
-          "border-width": "0"
-        },
-        "background-color": "white",
-        "item": {
-          "cursor": "hand"
-        },
-        "toggle-action": "remove"
-      },
-      "scaleX": {
-        "values": scaleXvalues,
-        "max-items": 8
-      },
-      "scaleY": {
-        "line-color": "#333"
-      },
-      "tooltip": {
-        "text": "%t: %v outbreaks in %k"
-      },
-      "plot": {
-        "line-width": 3,
-        "marker": {
-          "size": 2
-        },
-        "selection-mode": "multiple",
-        "background-mode": "graph",
-        "selected-state": {
-          "line-width": 4
-        },
-        "background-state": {
-          "line-color": "#eee",
-          "marker": {
-            "background-color": "none"
-          }
-        }
-      },
-      "plotarea": {
-        "margin": "15% 25% 10% 7%"
-      },
-      "series": [{
-        "values": NumOfDetections,
-        "text": "NumOfDetections",
-        "line-color": "#a6cee3",
-        "marker": {
-          "background-color": "#a6cee3",
-          "border-color": "#a6cee3"
-        }
-      }, {
-        "values": [148, 137, 149, 134, 132, 136, 141, 115, 120, 146, 117, 118, 132, 114, 116],
-        "text": "Salmonella",
-        "line-color": "#1f78b4",
-        "marker": {
-          "background-color": "#1f78b4",
-          "border-color": "#1f78b4"
-        }
-      }, {
-        "values": [27, 53, 28, 11, 32, 38, 35, 33, 33, 13, 6, 4, 2, 17, 13],
-        "text": "Other"
-      }]
-    };
+    google.charts.setOnLoadCallback(drawChart); 
+    function drawChart() {
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Date');
+		data.addColumn('number', 'Detections');
+		data.addRows(rows);
 
-    zingchart.render({
-      id: 'Monitor',
-      data: myConfig,
-      height: 400,
-      width: 1000
-    });
-	
-	
+	// Set chart options
+	var options = {
+		'width':700,
+		'height':400,
+		chartArea: { width: 380 },
+	};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('Monitor'));
+        chart.draw(data, options);
+      }
+	  
 }
