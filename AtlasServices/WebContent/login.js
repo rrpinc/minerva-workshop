0b3b747c2f7faa7d3b9ecfa7f59cb96b539daf58
Alon Shomrat <shomratalon@gmail.com>
2016-03-07 22:07:40 +0200

    hardcoded to relative

--- AtlasServices/WebContent/login.js
+++ AtlasServices/WebContent/login.js
@@ -1,18 +1,11 @@
 
-//$(document).ready(function() {
-//
-//	$('#login').on('click',checkLogin);
-//	
-//});
-
-
 function ajaxLogin(credentials) {
     var retData = null;
     $.ajax({
         async: false,
         type: 'GET',
         data: credentials,
-        url: "http://localhost:8080/AtlasServices/Login",
+        url: "Login",
         success: function(data) {
             retData = data;
         }
@@ -21,9 +14,7 @@ function ajaxLogin(credentials) {
 }
 
 function checkLogin(encrypted, key, s, i) {
-//function checkLogin() {
     var credentials = {email: $("#email").val(), pass: encrypted, key: key, salt: s, iv: i };
-	//var credentials = {email: $("#email").val(), pass: $("#pwd").val()};
 
 	var res = window.ajaxLogin(credentials);
     if (res["userlogged"] !== "true") {
@@ -33,7 +24,7 @@ function checkLogin(encrypted, key, s, i) {
 		localStorage.setItem('loggedin', false);
     } else {
     	localStorage.setItem('loggedin', true);
-        window.location = "http://localhost:8080/AtlasServices/main.html"; 
+        window.location = "main.html"; 
     }
 }
 	
